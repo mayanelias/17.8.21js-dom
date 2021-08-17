30ffb1ca9c7a091343d609ba888a612ed961679f
mayanelias <eliasmayan@gmail.com>
2021-08-17 21:32:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,141 @@
+//1//
+// but.addEventListener("click",function(){
+//    console.log(but.innerText= "click me");
+// });
+//2//
+// const button = document.getElementsByClassName("1");
+// console.log(button);
+// for (let i = 0; i < button.length; i++) {
+//     button[i].addEventListener("click", function () {
+//        if(i==0){
+//            alert("hi")
+//        }
+//        if(i==1){
+//         alert("my")
+//     }
+//     if(i==2){
+//         alert("name")
+//     }
+//     if(i==3){
+//         alert("mayan")
+//     }
+//     })
+// }
+//3//
+// const button = document.getElementsByClassName("li");
+// console.log(button);
+// for (let i = 0; i < button.length; i++) {
+//     button[i].addEventListener("click", function () {
+//         if(i==0){
+//                        console.log("mayan");
+//                    }
+//                    if(i==1){
+//                     console.log("ezra");
+//                 }
+//                 if(i==2){
+//                     console.log("nadav");
+//                 }
+//                 if(i==3){
+//                     console.log("ruti");
+//                 }
+//              if(i==4){
+//                     console.log("anat");
+//                 }
+//                 })
+// } 
+//4// 
+// let num=0;
+// but.addEventListener("click",function(){
+//     num=p.innerText++
+    
+//  });
+//5//
+// let num=0;
+// butt.addEventListener("click",function(){
+//     num=ph.innerText--;
+//  });
+//6//
+// const arrayManagment = [
+//     {
+//         id: 311532212,
+//         name: "daniel elias",
+//         phone: "0527302518",
+//         email: "eliasdaniel@gmail.com",
+//         balance: 1000000
+//     },
+//     {
+//         id: 311532211,
+//         name: "gil elias",
+//         phone: "0537302513",
+//         email: "eliasgil@gmail.com",
+//         balance: 300
+//     },
+//     {
+//         id: 311532210,
+//         name: "mayan elias",
+//         phone: "0547302514",
+//         email: "eliasmayan@gmail.com",
+//         balance: 100
+//     }
+// ]
+// let arrayMore400=[];
+// for(let i=0;i<arrayManagment.length;i++){
+//     if(arrayManagment[i].balance>400){
+//     arrayMore400.push(arrayManagment[i])
+//     }
+// }
+// console.log(arrayMore400);
+// let arrayLess200=[];
+// for(let i=0;i<arrayManagment.length;i++){
+//     if(arrayManagment[i].balance<200){
+//     arrayLess200.push(arrayManagment[i])
+//     }
+// }
+// console.log(arrayLess200);
+// const arrayId = [1234567891, 1234567892, 1234567893];
+// function block(arrayId) {
+//     let userInput = Number(prompt("enter id"));
+//     for (let i = 0; i < arrayId.length; i++) {
+//         if (userInput == arrayId[i]) {
+//             alert("you arev block");
+//         }
+//     } 
+// }
+// function clientWithMilion(){
+// for(let i=0;i<arrayManagment.length;i++){
+//     if(arrayManagment[i].balance==1000000){
+//         return arrayManagment[i]; 
+
+//     }
+// }
+// }
+// console.log(clientWithMilion());
+// block(arrayId)
+//7//
+// const product=
+// [{name:"bread",price:20,discription:"very tastey"},
+// {name:"coke",price:10,discription:"very sweet"},
+// {name:"paper",price:25,discription:"clean the house"}
+// ];
+// let max=product[0].price;
+// for(let i=0;i<product.length;i++){
+//     if(product[i].price>max){
+//         max=product[i].price;
+//     }
+// }
+// console.log(max);
+// let min=product[0].price;
+// for(let i=0;i<product.length;i++){
+//     if(product[i].price<min){
+//         min=product[i].price;
+//     }
+
+// }
+// console.log(min);
+// for(let i=0;i<product.length;i++){
+//     product[i].price*=0.7;
+//     console.log(product[i].price);
+// }
+
+
+
